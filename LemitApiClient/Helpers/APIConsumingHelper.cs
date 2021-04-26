4d7b225a5f0a4bb21b278004370703380d6d4f96
William Santana <williamss@outlook.com.br>
2021-04-26 13:52:02 -0300

    Adicionar arquivos de projeto.

---
+++ LemitApiClient/Helpers/APIConsumingHelper.cs
@@ -0,0 +1,64 @@
+﻿using ManyHelpers.Strings;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemitApiClient.Helpers {
+    public class ApiConsumerHelper {
+        private HttpClient _client;
+        private string _baseAdress;
+
+        public ApiConsumerHelper(string baseAdress, string token) {
+            _baseAdress = baseAdress;
+
+            var handler = new HttpClientHandler {
+                ServerCertificateCustomValidationCallback = (requestMessage, certificate, chain, policyErrors) => true
+            };
+
+            _client = new HttpClient(handler);
+            _client.DefaultRequestHeaders.TryAddWithoutValidation("Token", token);
+            _client.Timeout = TimeSpan.FromMinutes(30);
+        }
+
+        public (T result, string statusCode, string message) Get<T>(string endPoint) {
+            return GetAssync<T>(endPoint).Result;
+        }
+
+        public async Task<(T result, string statusCode, string message)> GetAssync<T>(string endPoint) {
+            try {
+                var response = await _client.GetAsync($"{_client.BaseAddress}{endPoint}");
+                var header = response.Headers;
+                return DeserializeResponse<T>(response);
+            } catch (Exception e) {
+                return (default(T), null, e.Message);
+            }
+        }
+
+        public (T result, string statusCode, string message) DeserializeResponse<T>(HttpResponseMessage response) {
+            var responseContent = response.Content.ReadAsStringAsync().Result;
+            var statusCode = response.StatusCode.ToString();
+            try {
+                if (response.IsSuccessStatusCode) {
+                    return (JsonConvert.DeserializeObject<T>(responseContent), statusCode, responseContent);
+                } else {
+                    return (default(T), statusCode, responseContent);
+                }
+            } catch (Exception e) {
+                return (default(T), statusCode, responseContent);
+            }
+
+        }
+
+        public HttpContent ObjectToHttpContent(object obj) {
+            if (obj.GetType().IsPrimitive || obj.GetType() == typeof(string) || obj.GetType() == typeof(decimal)) {
+                return new StringContent(obj.ToString());
+            }
+            string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            return new StringContent(json, Encoding.UTF8, "application/json");
+        }
+    }
+}
---
+++ LemitApiClient/LemitApi.cs
@@ -0,0 +1,19 @@
+﻿using LemitApiClient.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemitApiClient {
+    public class LemitApi {
+        private ApiConsumerHelper _consumingApiHelper;
+        private string _baseApi = "https://lemitti.com/api/v1/consulta/";
+
+        public LemitApi(string apiKey) {
+            _consumingApiHelper = new ApiConsumerHelper(_baseApi, apiKey);
+        }
+
+
+    }
+}
